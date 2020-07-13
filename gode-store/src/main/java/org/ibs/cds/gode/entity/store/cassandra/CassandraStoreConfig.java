ab1b99d1421d10ade502ff43d0fa0ebbf4ec78b5
Arfullahi Aliyar <v-aaliyar@expedia.com>
2020-07-13 12:06:21 +0530

    Trello:13-database-support-cassandra Cassandra data base implimentation

---
+++ gode-store/src/main/java/org/ibs/cds/gode/entity/store/cassandra/CassandraStoreConfig.java
@@ -0,0 +1,71 @@
+package org.ibs.cds.gode.entity.store.cassandra;
+
+import com.datastax.driver.core.AuthProvider;
+import com.datastax.driver.core.PlainTextAuthProvider;
+import org.ibs.cds.gode.entity.store.MarkJPARepo;
+import org.ibs.cds.gode.entity.store.condition.CassandraStoreEnabler;
+import org.ibs.cds.gode.entity.store.condition.JPAStoreEnabler;
+import org.ibs.cds.gode.system.GodeConstant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.*;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@Conditional(CassandraStoreEnabler.class)
+@EnableJpaRepositories(basePackages = GodeConstant.ENTITY_BASE_PACKAGE_NAME,
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MarkJPARepo.class)
+        })
+@ComponentScan(GodeConstant.GODE_BASE_PACKAGE_NAME)
+@PropertySource(GodeConstant.GODE_PROPERTIES)
+@EntityScan(GodeConstant.ENTITY_BASE_PACKAGE_ALL)
+public class CassandraStoreConfig extends AbstractCassandraConfiguration {
+
+    @Value("${gode.datastore.cassandra.contact-points:placeholder}")
+    private String contactPoints;
+
+    @Value("${gode.datastore.cassandra.port:0000}")
+    private int port;
+
+    @Value("${gode.datastore.cassandra.keyspace:placeholder}")
+    private String keySpace;
+
+    @Value("${gode.datastore.cassandra.username}")
+    private String username;
+
+    @Value("${gode.datastore.cassandra.password}")
+    private String password;
+
+    @Value("${gode.datastore.cassandra.schema-action}")
+    private String schemaAction;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keySpace;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.valueOf(schemaAction);
+    }
+
+    @Override
+    protected AuthProvider getAuthProvider() {
+        return new PlainTextAuthProvider(username, password);
+    }
+}
---
+++ gode-store/src/main/java/org/ibs/cds/gode/entity/store/cassandra/repo/CassandraSpringRepo.java
@@ -0,0 +1,23 @@
+package org.ibs.cds.gode.entity.store.cassandra.repo;
+
+import org.ibs.cds.gode.entity.type.CassandraEntity;
+import org.ibs.cds.gode.entity.type.JPAEntity;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+@NoRepositoryBean
+public interface CassandraSpringRepo<Entity extends CassandraEntity<Id>,Id extends Serializable> extends CassandraRepository<Entity, Id> {
+
+    Stream<Entity> findByActive(boolean active);
+    Page<Entity> findByActive(boolean active, Pageable pageable);
+    Optional<Entity> findByAppId(Long appId);
+    List<Entity> findAllById(Iterable<Id> ids);
+    Optional<Entity> findById(Id id);
+}
