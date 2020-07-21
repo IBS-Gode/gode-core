61ae817cefef6f119e2de3d5ce05e182d0ee09fb
manugraj <manu.raj@ibsplc.com>
2020-07-21 20:54:29 +0530

    Minor refactoring for centralised versioning

--- gode-store/src/main/java/org/ibs/cds/gode/entity/store/mongo/MongoDBStoreConfig.java
+++ gode-store/src/main/java/org/ibs/cds/gode/entity/store/mongo/MongoDBStoreConfig.java
@@ -1,28 +1,27 @@
 package org.ibs.cds.gode.entity.store.mongo;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.ibs.cds.gode.entity.codec.DateToOffsetDateTimeConverter;
-import org.ibs.cds.gode.entity.codec.OffsetDateTimeToDateConverter;
 import org.ibs.cds.gode.entity.store.MarkMongoRepo;
 import org.ibs.cds.gode.entity.store.condition.MongoDBEnabler;
 import org.ibs.cds.gode.system.GodeAppEnvt;
 import org.ibs.cds.gode.system.GodeConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.*;
-import org.springframework.core.convert.converter.Converter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.List;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 
 @Configuration
@@ -38,15 +37,10 @@ import java.util.List;
 public class MongoDBStoreConfig extends AbstractMongoClientConfiguration {
 
     private final Environment environment;
-    private final List<Converter<?, ?>> codecList;
 
     @Autowired
     public MongoDBStoreConfig(Environment environment) {
-        this.environment = environment;
-        codecList = List.of(
-                new DateToOffsetDateTimeConverter(),
-                new OffsetDateTimeToDateConverter()
-        );
+        this.environment = environment;  
     }
 
     @Override
@@ -66,18 +60,13 @@ public class MongoDBStoreConfig extends AbstractMongoClientConfiguration {
 
     }
 
-    @Override
-    public MongoCustomConversions customConversions() {
-        return new MongoCustomConversions(codecList);
-    }
-
     @Bean
     public GridFsTemplate gridFsTemplate() throws Exception {
-        return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
+        return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter(mongoDbFactory(), customConversions(), mongoMappingContext(customConversions()) ));
     }
 
-    @Bean
-    public MongoTransactionManager mongoTransactionManager(MongoDbFactory dbFactory) {
-        return new MongoTransactionManager(dbFactory);
+    @Bean 
+    public MongoTransactionManager mongoTransactionManager() {
+        return new MongoTransactionManager(mongoDbFactory());
     }
 }
